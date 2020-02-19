7090db39074ca1795b77596f5b86823dbbd370ad
Xuantho01 <hoxuantho95@gmail.com>
2020-02-19 22:55:01 +0700

    inheritance

---
+++ src/Point2D.java
@@ -0,0 +1,42 @@
+import java.util.Arrays;
+
+public class Point2D {
+    private float x = 0.0f;
+    private float y = 0.0f;
+    float[] array = new float[2];
+    public Point2D(){}
+    public Point2D(float x, float y){
+        this.x = x;
+        this.y = y;
+    }
+    public float getX() {
+        return x;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+}
+
+    public float getY() {
+        return y;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+
+    public void setXY(float x, float y) {
+        array[0] = x;
+        array[1] = y;
+    }
+
+    public float[] getXY(){
+        return array;
+    }
+    @Override
+    public String toString(){
+        return "array: "+ array;
+    }
+
+
+}
---
+++ src/Point2DTest.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class Point2DTest {
+    public static void main(String[] args) {
+        Point2D point2D = new Point2D();
+        point2D.setXY(10,12);
+        System.out.println(point2D);
+//        System.out.println("array"+ Arrays.toString(point2D.getXY()));
+//        point2D.setXY(10,10);
+//        System.out.println("array"+ Arrays.toString(point2D.getXY()));
+    }
+}
